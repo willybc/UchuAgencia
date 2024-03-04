4ab7bd52ed555948bd2ddab2a59eebf9041f522b
Willy Bardales <willybc06@gmail.com>
2024-03-03 23:38:34 -0300

    added video

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -6,7 +6,7 @@ export class ConfigService {
 
 	load(): Promise<void> {
         return new Promise((resolve) => {
-            setTimeout(resolve, 5000);
+            setTimeout(resolve, 7000);
         })
     }
 }
