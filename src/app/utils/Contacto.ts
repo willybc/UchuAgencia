50a87f8b1d733588c874bccb11a6c3a917d076bc
Willy Bardales <willybc06@gmail.com>
2024-03-14 19:32:44 -0300

    added whatsapp number

--- src/app/utils/Contacto.ts
+++ src/app/utils/Contacto.ts
@@ -2,5 +2,5 @@ export class Contacto {
 	static email = 'hello@uchuagencia.com';
 	static linkedin = 'https://www.linkedin.com/in/uchu-agencia';
 	static instagram = 'https://www.instagram.com/uchuagencia?igsh=amVsNTExOG83OWky';
-	static whatsapp = 'https://wa.me/1234567890';
+	static whatsapp = 'https://api.whatsapp.com/send?phone=+5491161398185&text=Quiero%20m%C3%A1s%20informaci%C3%B3n';
 }
